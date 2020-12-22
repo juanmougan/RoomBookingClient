d014a3e2537c3c1f6f8515faefc289761e3b7aae
Juan Manuel Mougan <juanmougan@gmail.com>
2020-12-22 15:21:46 -0300

    Unit 3

--- src/app/admin/rooms/rooms.component.ts
+++ src/app/admin/rooms/rooms.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from 'src/app/data.service';
+import { Room } from 'src/app/model/Room';
 
 @Component({
   selector: 'app-rooms',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomsComponent implements OnInit {
 
-  constructor() { }
+  rooms: Array<Room>;
+  selectedRoom: Room;
+
+  constructor(private dataService: DataService, 
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.rooms = this.dataService.rooms;
+    this.route.queryParams.subscribe(
+      (params) => {
+        const id = params['id'];
+        if(id) {
+          this.selectedRoom = this.rooms.find( r => {
+            return r.id === +id;  // The '+' is a hack to convert String to Number
+          } );
+        }
+      }
+    );
+  }
+
+  setRoom(id: number) {
+    // The queryParams are provided as an object
+    this.router.navigate(['admin', 'rooms'], { queryParams: { id: id } });
   }
 
 }
--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from 'src/app/data.service';
+import { User } from 'src/app/model/User';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users: Array<User>;
+  selectedUser: User;
+
+  constructor(private dataService: DataService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.users = this.dataService.users;
+    this.route.queryParams.subscribe(
+      (params) => {
+        const id = params['id'];
+        if(id) {
+          this.selectedUser = this.users.find( r => {
+            return r.id === +id;  // The '+' is a hack to convert String to Number
+          } );
+        }
+      }
+    );
+  }
+
+  setUser(id: number) {
+    // The queryParams are provided as an object
+    this.router.navigate(['admin', 'users'], { queryParams: { id } }); // Same as "id: id"
   }
 
 }
