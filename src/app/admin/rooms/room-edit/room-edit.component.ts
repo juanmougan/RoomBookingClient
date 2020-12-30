e7594a9538cf888429ef56d6835ed25bd17cb0f9
Juan Manuel Mougan <juanmougan@gmail.com>
2020-12-30 16:41:33 -0300

    Finished unit 7

--- src/app/admin/rooms/room-edit/room-edit.component.ts
+++ src/app/admin/rooms/room-edit/room-edit.component.ts
@@ -1,19 +1,52 @@
+import { Layout, LayoutCapacity } from './../../../model/Room';
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Room } from 'src/app/model/Room';
 
 @Component({
   selector: 'app-room-edit',
   templateUrl: './room-edit.component.html',
-  styleUrls: ['./room-edit.component.css']
+  styleUrls: ['./room-edit.component.css'],
 })
 export class RoomEditComponent implements OnInit {
-
   @Input()
   room: Room;
-  
-  constructor() { }
+
+  layouts = Object.keys(Layout);
+  layoutEnum = Layout; // To use the enum in the HTML
+
+  roomForm = new FormGroup({
+    roomName: new FormControl('roomName'),
+    location: new FormControl('location'),
+  });
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.roomForm.patchValue({
+      roomName: this.room.name,
+      location: this.room.location,
+    });
+
+    for (const layout of this.layouts) {
+      this.roomForm.addControl(
+        `layout${layout}`,
+        new FormControl(`layout${layout}`)
+      );
+    }
   }
 
+  onSubmit() {
+    this.room.name = this.roomForm.controls['roomName'].value;
+    this.room.location = this.roomForm.value['location']; // Both alternatives work
+    this.room.capacities = new Array<LayoutCapacity>();
+    for (const layout of this.layouts) {
+      const layoutCapacity = new LayoutCapacity();
+      layoutCapacity.layout = Layout[layout];
+      layoutCapacity.capacity = this.roomForm.value[`layout${layout}`];
+      this.room.capacities.push(layoutCapacity);
+      // TODO add a save method on the dataservice
+      console.log(this.room);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,15 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { RoomDetailComponent } from './admin/rooms/room-detail/room-detail.component';
 import { UserDetailComponent } from './admin/users/user-detail/user-detail.component';
 import { UserEditComponent } from './admin/users/user-edit/user-edit.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RoomEditComponent } from './admin/rooms/room-edit/room-edit.component';
 
 const routes: Routes = [
-  {path : `admin/users`, component : UsersComponent},
-  {path : `admin/rooms`, component : RoomsComponent},
-  {path : ``, component : CalendarComponent},
-  {path : `404`, component : PageNotFoundComponent},
-  {path : `**`, redirectTo : `/404`}  // Wildcard must be at the end, matches all
+  { path: `admin/users`, component: UsersComponent },
+  { path: `admin/rooms`, component: RoomsComponent },
+  { path: ``, component: CalendarComponent },
+  { path: `404`, component: PageNotFoundComponent },
+  { path: `**`, redirectTo: `/404` }, // Wildcard must be at the end, matches all
 ];
 
 @NgModule({
@@ -33,14 +33,15 @@ const routes: Routes = [
     RoomDetailComponent,
     UserDetailComponent,
     UserEditComponent,
-    RoomEditComponent
+    RoomEditComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
